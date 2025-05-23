e591b7ebc74b7e476ef1189f2c07fe6337fc6150
Alexis DUCHET <alexis.duchet@epita.fr>
2025-05-23 19:00:43 +0200

    remove horrible designer

--- grape/controllers/wine_controller.py
+++
@@ -1,8 +0,0 @@
-from models.wine_model import Wine
-
-def add_new_wine(name, year, type, quantity):
-    wine = Wine(name, int(year), type, int(quantity))
-    Wine.add_wine(wine)
-
-def get_all_wines():
-    return Wine.get_all_wines()
